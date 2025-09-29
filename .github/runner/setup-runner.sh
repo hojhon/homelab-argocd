714d9bddcaf3ba2aef3eb3604155514fcb58dd2d
hojhon <115334790+hojhon@users.noreply.github.com>
2025-09-29 01:43:48 -0400

    Update setup-runner.sh

--- .github/runner/setup-runner.sh
+++ .github/runner/setup-runner.sh
@@ -98,7 +98,7 @@ sudo -u github-runner bash -c 'cd /home/github-runner/actions-runner && ./config
     --url https://github.com/hojhon/homelab-argocd \
     --token "'${RUNNER_TOKEN}'" \
     --name "dev-ops" \
-    --labels "self-hosted,k3s,proxmox,dev-ops" \
+    --labels "self-hosted,dev-ops" \
     --work _work \
     --runasservice'
 
